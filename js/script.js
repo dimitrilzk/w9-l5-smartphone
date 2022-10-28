5c09047027d55b129a53f102e5b1fda8c6df94bc
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-28 16:01:56 +0200

    soddisfatto dei progressi

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 "use strict";
 class Smartphone {
-    constructor(credito, minutiChiamata) {
+    constructor(credito, minutiChiamata, tariffa) {
         this.credito = 0;
         this.minutiChiamata = 0;
+        this.tariffa = 0;
         this.credito = credito;
         this.minutiChiamata = minutiChiamata;
+        this.tariffa = tariffa;
     }
-    ricarica() {
-        return this.credito += 20;
+    ricarica(soldi) {
+        return this.credito += soldi;
     }
-    durataChiamate() {
-        return this.minutiChiamata += 3;
+    durataChiamata(minuti) {
+        return this.minutiChiamata += minuti;
     }
     creditoResiduo() {
-        return this.credito;
+        let res = this.credito - this.minutiChiamata * this.tariffa;
+        //console.log(`Il tuo credito è ${res}`);
     }
     mostraDurataChiamate() {
         return this.minutiChiamata;
@@ -22,3 +25,30 @@ class Smartphone {
         return this.minutiChiamata = 0;
     }
 }
+class Vodafone extends Smartphone {
+    ricarica(soldi) {
+        console.log(this.credito += soldi);
+    }
+    calling(minutes) {
+        if (this.credito >= minutes * 0.2) {
+            this.minutiChiamata += minutes;
+            this.credito -= minutes * 0.2;
+        }
+        else {
+            console.log(`Gentile utente il tuo credito è insufficiente, attualmente hai ${this.credito}, fai una ricarica.`);
+        }
+    }
+}
+let user1 = new Vodafone(0, 0, 0.2);
+// user1.credito += 25;
+// user1.durataChiamata(8);
+// user1.creditoResiduo();
+user1.ricarica(50);
+user1.calling(5);
+console.log(user1);
+// class Wind extends Smartphone{
+// }
+// let user2 = new Samsung(0,0,0.2);
+// user2.ricarica(20);
+// user2.durataChiamata(5)
+// console.log(user2)
--- ts/script.ts
+++ ts/script.ts
@@ -8,18 +8,21 @@ interface ISmartphone {
 abstract class Smartphone {
     credito: number = 0;
     minutiChiamata: number = 0;
-    constructor(credito: number, minutiChiamata: number) {
+    tariffa: number = 0;
+    constructor(credito: number, minutiChiamata: number, tariffa: number) {
         this.credito = credito;
         this.minutiChiamata = minutiChiamata;
+        this.tariffa = tariffa;
     }
-    ricarica(): number {
-       return this.credito += 20;
+    ricarica(soldi:number): number {
+       return this.credito += soldi;
     }
-    durataChiamate(): number {
-        return this.minutiChiamata += 3;
+    durataChiamata(minuti:number): number {
+        return this.minutiChiamata += minuti;
     }
-    creditoResiduo(): number {
-        return this.credito;
+    creditoResiduo(): any {
+        let res = this.credito - this.minutiChiamata * this.tariffa;
+        //console.log(`Il tuo credito è ${res}`);
     }
     mostraDurataChiamate(): number {
         return this.minutiChiamata;
@@ -27,5 +30,36 @@ abstract class Smartphone {
     azzeraChiamate(): number {
         return this.minutiChiamata = 0;
     }
+}
+class Vodafone extends Smartphone {
+    ricarica(soldi:number): any {
+        console.log(this.credito += soldi);
+     }
+    calling(minutes: number): any {
+        if(this.credito >= minutes *0.2) {
+            this.minutiChiamata += minutes;
+            this.credito -= minutes *0.2;
+        } else {
+            console.log(`Gentile utente il tuo credito è insufficiente, attualmente hai ${this.credito}, fai una ricarica.`)
+        }
+    }
 
 }
+let user1 = new Vodafone(0,0,0.2);
+// user1.credito += 25;
+// user1.durataChiamata(8);
+// user1.creditoResiduo();
+user1.ricarica(50)
+user1.calling(5);
+console.log(user1)
+
+
+
+
+// class Wind extends Smartphone{
+
+// }
+// let user2 = new Samsung(0,0,0.2);
+// user2.ricarica(20);
+// user2.durataChiamata(5)
+// console.log(user2)

5fa5f406b5dd8867355a29a36eda17eac551b6f5
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-28 12:35:23 +0200

    class smartphone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+class Smartphone {
+    constructor(credito, minutiChiamata) {
+        this.credito = 0;
+        this.minutiChiamata = 0;
+        this.credito = credito;
+        this.minutiChiamata = minutiChiamata;
+    }
+    ricarica() {
+        return this.credito += 20;
+    }
+    durataChiamate() {
+        return this.minutiChiamata += 3;
+    }
+    creditoResiduo() {
+        return this.credito;
+    }
+    mostraDurataChiamate() {
+        return this.minutiChiamata;
+    }
+    azzeraChiamate() {
+        return this.minutiChiamata = 0;
+    }
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,7 +1,31 @@
 interface ISmartphone {
     ricarica(): number;
     durataChiamate(): number;
-    creidtoResiduo(): number;
-    getDurataChiamate(): number;
+    creditoResiduo(): number;
+    mostraDurataChiamate(): number;
     azzeraChiamte(): number;
+}
+abstract class Smartphone {
+    credito: number = 0;
+    minutiChiamata: number = 0;
+    constructor(credito: number, minutiChiamata: number) {
+        this.credito = credito;
+        this.minutiChiamata = minutiChiamata;
+    }
+    ricarica(): number {
+       return this.credito += 20;
+    }
+    durataChiamate(): number {
+        return this.minutiChiamata += 3;
+    }
+    creditoResiduo(): number {
+        return this.credito;
+    }
+    mostraDurataChiamate(): number {
+        return this.minutiChiamata;
+    }
+    azzeraChiamate(): number {
+        return this.minutiChiamata = 0;
+    }
+
 }
